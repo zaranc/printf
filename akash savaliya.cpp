3b608d01e3f91f38b626125aca81edcf2cdc8ee9
zaranc <130464977+zaranc@users.noreply.github.com>
2023-04-11 17:39:42 +0530

    Add files via upload

---
+++ akash savaliya.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+main()
+{
+	printf("---"\n);
+	printf("|    |\n");
+    printf("a    |\n");
+    printf("k    |\n");
+    printf("a    |\n");
+    printf("s    |\n");
+    printf("h    |\n");
+    printf("  ---   |\n");
+}
